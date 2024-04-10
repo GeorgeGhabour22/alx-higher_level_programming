18a02723af004c94188ca3f80bce138a4059bddf
GeorgeGhabour22 <95974387+GeorgeGhabour22@users.noreply.github.com>
2024-04-10 23:41:26 +0200

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-// This line specifies the path to the interpreter for the script.
+// prints the first argument passed to it
+
 if (process.argv[2] === undefined) {
-  // Checks if there is no argument provided when running the script.
   console.log('No argument');
-  // If there's no argument, outputs the string 'No argument' to the console.
 } else {
   console.log(process.argv[2]);
-  // If an argument is provided, outputs the value of the argument
-  // (at index 2 in the process.argv array) to the console.
 };
-
