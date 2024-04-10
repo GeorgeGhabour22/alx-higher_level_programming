ecc39facae423bbe9231bb0ba3ef2b60e35a43d0
GeorgeGhabour22 <95974387+GeorgeGhabour22@users.noreply.github.com>
2024-04-10 23:40:09 +0200

    Update 13-add.js

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-
-// This line specifies the path to the interpreter for the script.
-
-exports.add = function (a, b) {
-  // This line exports a function named "add" with parameters "a" and "b".
-  return (a + b);
-  // This function returns the sum of the parameters "a" and "b".
+const myObject = {
+  type: 'object',
+  value: 12
 };
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
