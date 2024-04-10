7d101ac25a8785c2b66372f92b5e60e077e59b41
GeorgeGhabour22 <95974387+GeorgeGhabour22@users.noreply.github.com>
2024-04-10 23:37:55 +0200

    Update 100-let_me_const.js

--- 0x12-javascript-warm_up/100-let_me_const.js
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -1,9 +1,3 @@
-
 #!/usr/bin/node
-
-// This line specifies the path to the interpreter for the script.
-
+// modifies the value of myVar to 333
 myVar = 333;
-// This line defines and initializes the constant variable,
-// "myVar" with the value 333.
-
