f6cfe2e3c9846bca68da39929f50e7760eb19c32
GeorgeGhabour22 <95974387+GeorgeGhabour22@users.noreply.github.com>
2024-04-10 23:41:50 +0200

    Update 4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-
-// This line specifies the path to the interpreter for the script.
-
+// prints two arguments passed to it, in the following format: “ is ”
 console.log(process.argv[2] + ' is ' + process.argv[3]);
-// This line accesses command-line arguments using the "process.argv" array.
-// It concatenates the value at index 2 with the string ' is ' and the value,
-// at index 3, then outputs the resulting string to the console.
-// This can be used to display a custom message using command-line arguments.
