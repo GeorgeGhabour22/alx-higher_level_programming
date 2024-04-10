5feeba1ab53f16eabdc34200cd9a90f11d3d19d7
GeorgeGhabour22 <95974387+GeorgeGhabour22@users.noreply.github.com>
2024-04-10 23:37:01 +0200

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,12 +1,3 @@
 #!/usr/bin/node
-
-// This line specifies the path to the interpreter for the script.
-
-console.log('C is fun');
-// This line outputs the string 'C is fun' to the console.
-
-console.log('Python is cool');
-// This line outputs the string 'Python is cool' to the console.
-
-console.log('JavaScript is amazing');
-// This line outputs the string 'JavaScript is amazing' to the console.
+// It prints 3 lines
+console.log('C is fun\nPython is cool\nJavascript is amazing');
